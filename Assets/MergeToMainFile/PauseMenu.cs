eb91d6c69d6255f5af00511d89328e023efdde89
wyhoi <0120573@kdu-online.com>
2019-04-17 18:09:53 +0800

    Added PauseMenu n script, added value to leftbound & rightbound

---
+++ Assets/MergeToMainFile/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ToSetting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+}
