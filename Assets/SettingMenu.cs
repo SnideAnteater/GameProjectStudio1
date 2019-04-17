150a7d603c506a608d68cffb37bad01c0b365d09
SnideAnteater <totsmcgeee@gmail.com>
2019-04-18 05:32:35 +0800

    Updates on Sounds

---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioSource audioSc;
+    private float musicVolume = 1f;
+
+    void Start()
+    {
+        audioSc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audioSc.volume = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+}
