1193931cc3a1cc40f166e1a694bf8ed59cdf97a7
SnideAnteater <totsmcgeee@gmail.com>
2019-04-18 07:03:50 +0800

    deadass dead wanna die dead 1 more hour wanna die and sleep eternally

--- Assets/MergeToMainFile/PauseMenu.cs
+++ Assets/MergeToMainFile/PauseMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject settingPanel;
 
     private void Start()
     {
@@ -21,11 +22,15 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         pausePanel.SetActive(true);
+        settingPanel.SetActive(false);
+        Time.timeScale = 0f;
     }
 
     public void Resume()
     {
+        Time.timeScale = 1f;
         pausePanel.SetActive(false);
+        settingPanel.SetActive(false);
     }
 
     public void BackToMenu()
@@ -35,6 +40,7 @@ public class PauseMenu : MonoBehaviour
 
     public void ToSetting()
     {
-        SceneManager.LoadScene("Setting");
+        settingPanel.SetActive(true);
+        pausePanel.SetActive(false);
     }
 }
---
+++ Assets/unlockStairs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unlockStairs : MonoBehaviour
+{
+    public GameObject stairsUnlock;
+    GameObject player;
+    public int puzzlesBeforeUnlock;
+    public string lockedMessage = "It's Locked.";
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void Interact()
+    {
+        if (GameManager.Instance.PuzzleSolvedCount() >= puzzlesBeforeUnlock)
+        {
+            stairsUnlock.SetActive(true);
+            gameObject.SetActive(false);
+
+        }
+        else
+            GenericPrompt.Instance.ShowPrompt(lockedMessage);
+    }
+}
